8578cc8851535b3cb477421297fb48f3cd74e57e
Nico Audy <nicoaudy1994@gmail.com>
2020-11-27 14:31:47 +0700

    maintenance mode

--- public/index.php
+++ public/index.php
@@ -6,8 +6,11 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
 define('LARAVEL_START', microtime(true));
+
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
+}
 
 /*
 |--------------------------------------------------------------------------
